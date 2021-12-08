aeddbbe703e081e9ef8381c70d3c32d5eea5a918
SriSivaC <balu148@gmail.com>
2021-12-08 15:38:57 +0530

    modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+let ExplitiveContext = React.createContext("shit");
+// const OrganizationContext = React.createContext();
+// const PersonContext = React.createContext();
+
+const Exclamation = (props) => (
+  <h1>Oh, {props.word}!!!</h1>
+);
+
+function ContextualExclamation() {
+  return (
+    <ExplitiveContext>
+      {
+        word => <Exclamation word={word} />
+      }
+    </ExplitiveContext>
+  )
+}
+
+const GrandmasHouse = (props) => (
+  <ExplitiveContext.Provider value="poop">
+    {props.children}
+  </ExplitiveContext.Provider>
+)
+
+const App = () => (
+  <div>
+    <h1>
+      React Context
+    </h1>
+    <ContextualExclamation />
+    <ExplitiveContext.Provider value="poop">
+      <ContextualExclamation />
+    </ExplitiveContext.Provider>
+    <ExplitiveContext.Provider value="stink">
+      <ContextualExclamation />
+    </ExplitiveContext.Provider>
+    <GrandmasHouse>
+      <ContextualExclamation />
+    </GrandmasHouse>
+  </div>
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
